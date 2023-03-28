7fcf16ad384621ab76e4928cd02ff1a4fcf35332
Igor Lobach <lobach.igor.olegovich@gmail.com>
2023-03-28 19:24:48 +0300

    flake8 error

--- project/settings.py
+++ project/settings.py
@@ -10,28 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(
-    debug=True
-)
-
-environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY', default='SECRET_KEY')
+SECRET_KEY = 'django-insecure-psgo1zdslb-_pbp3-r*6*hoyox74z&wt25=gh=t(!gv7(_fpkm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True)
+DEBUG = True
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = []
 
 
 # Application definition
