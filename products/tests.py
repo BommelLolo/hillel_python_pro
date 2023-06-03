9fb161dd1466775c59712d4b412d5aaa9ed931e5
Igor Lobach <lobach.igor.olegovich@gmail.com>
2023-06-03 18:57:34 +0300

    pytest export csv, pdf

--- products/tests.py
+++ products/tests.py
@@ -1,7 +1,11 @@
+import os
+import PyPDF2
+
 from django.urls import reverse
 from random import randint
 
 from products.models import Product
+from project.settings import BASE_DIR
 
 
 def test_products_list(client, product_factory, faker):
@@ -12,9 +16,97 @@ def test_products_list(client, product_factory, faker):
     assert response.status_code == 200
     assert len(response.context['products']) == Product.objects.count()
 
-#
-# def test_product_detail(client, product_factory, faker):
-#     product_factory()
-#     url = reverse('products')
-#     response = client.get(url)
-#     assert response.status_code == 200
+
+def test_product_detail(client, product_factory, faker):
+    product_factory()
+    url = reverse('products')
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+
+def test_export_csv(client, product_factory, faker):
+    quantity = (randint(3, 20))
+    file_path = os.path.join(BASE_DIR, 'test_products.csv')
+    for _ in range(quantity):
+        product_factory()
+    url = reverse('products_to_csv')
+    response = client.get(url)
+    assert response.status_code == 200
+    attachment_data = response.content
+    with open(file_path, mode='wb') as f:
+        f.write(attachment_data)
+    f_content = []
+    with open(file_path, mode='r') as f:
+        for line in f:
+            f_content.append(line.strip())
+    assert f_content
+    assert len(f_content) == (quantity + 1)
+    os.remove(file_path)
+
+
+def test_export_to_pdf(client, product_factory, faker):
+    quantity = (randint(50, 100))
+    file_path = os.path.join(BASE_DIR, 'test_products.pdf')
+    for _ in range(quantity):
+        product_factory()
+    url = reverse('products_to_pdf')
+    response = client.get(url)
+    assert response.status_code == 200
+    attachment_data = response.content
+    with open(file_path, mode='wb') as f:
+        f.write(attachment_data)
+    pdf_file = open(file_path, 'rb')
+    # creating a pdf reader object
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    # printing number of pages in pdf file
+    pdf_content = []
+    rows = 0
+    for page in range(len(pdf_reader.pages)):
+        # creating a page object
+        page_obj = pdf_reader.pages[page]
+        pdf_content.append(page_obj.extract_text())
+        rows += len(pdf_content[page].split('\n'))
+    # closing the pdf file object
+    pdf_file.close()
+    assert rows == (quantity + 2)
+    os.remove(file_path)
+
+
+def test_import_csv(client, login_staff, product_factory, faker):
+    # creating products
+    quantity = (randint(3, 20))
+    file_path = os.path.join(BASE_DIR, 'test_products.csv')
+    for _ in range(quantity):
+        product_factory()
+    url = reverse('products_to_csv')
+    response = client.get(url)
+    assert response.status_code == 200
+    # creating csv file
+    attachment_data = response.content
+    with open(file_path, mode='wb') as f:
+        f.write(attachment_data)
+    url = reverse('products')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert len(response.context['products']) == Product.objects.count()
+    assert quantity == Product.objects.count()
+    Product.objects.all().delete()
+    url = reverse('products')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert len(response.context['products']) == Product.objects.count() == 0
+    # testing import csv
+    url = reverse('products_from_csv')
+    client, user = login_staff()
+    data = {
+        'file': file_path
+    }
+    breakpoint()
+    response = client.post(url, data=data)
+    assert response.status_code == 200
+    url = reverse('products')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert len(response.context['products']) == Product.objects.count()
+    os.remove(file_path)
